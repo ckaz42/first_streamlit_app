c915f8abadbcfa0c09348806c22c2ff6ad41dd19
Chaim Katz <46654856+ckaz42@users.noreply.github.com>
2022-06-03 08:50:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice')
 fruit_choice = streamlit.text_input('What fruit would you like to dilute?')
+streamlit.write('The user entered',fruit_choice)
+
+streamlit.stop() 
 if not fruit_choice:
   streamlit.error("Please choose a fruze to get information.")
 else:
